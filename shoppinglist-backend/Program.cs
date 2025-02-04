074373a48c6bbe12f5a82473b4bbabcf0061d0fd
Odd Fredrik Holen <ofholen@gmail.com>
2025-02-04 22:43:39 +0100

    Legger til cors i dev

--- shoppinglist-backend/Program.cs
+++ shoppinglist-backend/Program.cs
@@ -1,13 +1,32 @@
 using shoppinglist_backend;
 
 var builder = WebApplication.CreateBuilder(args);
-// Uten mellomrom
+
+// Uten mellomrom og רזו
 StaticConfiguration.Name = "DittNavn";
 
 // Add services to the container.
 builder.Services.AddSingleton<TableStorageService>();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "Dev",
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:3000")
+                            .AllowAnyHeader()
+                            .AllowAnyMethod();
+                      });
+});
+
 var app = builder.Build();
+
+bool isDevelopment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development";
+
+if (isDevelopment)
+{
+    app.UseCors("Dev");
+}
 
 app.MapGet("/api/shoppingitems", async (TableStorageService service) =>
 {
